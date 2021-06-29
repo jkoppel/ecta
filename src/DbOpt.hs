7c5e2adf9c8df8ab66a3ae8c4408417ad1120c59
Jack Feser <feser@csail.mit.edu>
2021-06-29 17:56:20 -0400

    Add more transformations.

--- src/DbOpt.hs
+++ src/DbOpt.hs
@@ -13,7 +13,7 @@ trinary name x y z = Node [Edge name [x, y, z]]
 binary :: Symbol -> Node -> Node -> Node
 binary name x y = Node [Edge name [x, y]]
 
-unary :: Symbol -> Node -> Node 
+unary :: Symbol -> Node -> Node
 unary name x = Node [Edge name [x]]
 
 const' name = Node [Edge name []]
@@ -21,8 +21,10 @@ consts names = Node $ List.map (\n -> Edge n []) names
 
 func name xs = Node [Edge name xs]
 
+none = const "none"
 filter' = binary "filter"
 join = trinary "join"
+depjoin = trinary "depjoin"
 eq = binary "eq"
 lt = binary "lt"
 dot = binary "dot"
@@ -30,6 +32,7 @@ name = const'
 relation = const'
 scope = consts ["k1", "k2", "k3"]
 hidx x y z = func "hidx" [x, y, z]
+meta = unary "meta"
 
 data Pattern =
   App Symbol [Pattern]
@@ -70,9 +73,12 @@ atNode n n' f =
 
 data Rule = Rule { lhs :: Pattern
                  , rhs :: Pattern
+                 , pred :: Node -> Bool
                  , constrs :: [EqConstraint]
                  }
-  deriving ( Show )
+
+instance Show Rule where
+  show Rule { lhs, rhs, constrs } = show (lhs, rhs, constrs)
 
 rewriteMatch :: PatternMatch -> Rule -> Node -> Node
 rewriteMatch PatternMatch { root, subst } rule n =
@@ -118,36 +124,88 @@ repeat' i r n =
   if i <= 0 then Just n else
   case r n of
     Nothing -> Just n
-    Just n' -> repeat' (i - 1) r n'
+    Just n' ->
+      if n == n' then Just n else repeat' (i - 1) r n'
 
 eqConstr p p' =
   EqConstraint (path p) (path p')
 
-filterToHidx = Rule {lhs, rhs, constrs}
+alwaysRule lhs rhs constrs = Rule { lhs, rhs, DbOpt.pred = const True, constrs }
+
+filterToHidx = alwaysRule lhs rhs constrs
   where
-    lhs = App "filter" [App "eq" [Var 0, Var 1], Var 2]
-    rhs = App "hidx" [App "select" [Var 0, Var 2], NodePat scope, App "filter" [App "eq" [Var 0, App "dot" [NodePat scope, Var 0]], Var 2, Var 1]]
+    ckey = Var 0
+    rkey = Var 1
+    rel = Var 2
+    lhs = App "filter" [App "eq" [ckey, rkey], rel]
+    rhs = App "hidx" [App "select" [ckey, rel], NodePat scope, App "filter" [App "eq" [ckey, App "dot" [NodePat scope, ckey]], rel, rkey]]
     constrs = [ eqConstr [1] [2, 0, 1, 0] ]
 
-filterToOidx = Rule {lhs, rhs, constrs}
+filterToOidx = alwaysRule lhs rhs constrs
   where
-    lhs = App "filter" [App "eq" [Var 0, Var 1], Var 2]
-    rhs = App "oidx" [App "select" [Var 0, Var 2], NodePat scope, App "filter" [App "eq" [Var 0, App "dot" [NodePat scope, Var 0]], Var 2, Var 1]]
+    ckey = Var 0
+    rkey1 = Var 1
+    rkey2 = Var 2
+    rel = Var 3
+    lhs = App "filter" [App "and" [App "lt" [rkey1, ckey], App "lt" [ckey, rkey2]], rel]
+    rhs = App "oidx" [ App "select" [ckey, rel],
+                       NodePat scope,
+                       App "filter" [App "eq" [ckey, App "dot" [NodePat scope, ckey]], rel, rkey1, rkey2]]
     constrs = [ eqConstr [1] [2, 0, 1, 0] ]
 
-splitFilter = Rule {lhs, rhs, constrs}
+filterToOidx1 = alwaysRule lhs rhs constrs
+  where
+    ckey = Var 0
+    rkey = Var 1
+    rel = Var 2
+    lhs = App "filter" [App "lt" [ckey, rkey], rel]
+    rhs = App "oidx" [ App "select" [ckey, rel],
+                       NodePat scope,
+                       App "filter" [App "eq" [ckey, App "dot" [NodePat scope, ckey]], rel, App "none" [], rkey]]
+    constrs = [ eqConstr [1] [2, 0, 1, 0] ]
+
+filterToOidx2 = alwaysRule lhs rhs constrs
+  where
+    ckey = Var 0
+    rkey = Var 1
+    rel = Var 2
+    lhs = App "filter" [App "lt" [rkey, ckey], rel]
+    rhs = App "oidx" [ App "select" [ckey, rel],
+                       NodePat scope,
+                       App "filter" [App "eq" [ckey, App "dot" [NodePat scope, ckey]], rel, rkey, App "none" []]]
+    constrs = [ eqConstr [1] [2, 0, 1, 0] ]
+
+splitFilter = alwaysRule lhs rhs constrs
   where
     lhs = App "filter" [App "and" [Var 0, Var 1], Var 2]
     rhs = App "filter" [Var 0, App "filter" [Var 1, Var 2]]
     constrs = []
 
-mergeFilter = Rule {lhs, rhs, constrs}
+mergeFilter = alwaysRule lhs rhs constrs
   where
     lhs = App "filter" [Var 0, App "filter" [Var 1, Var 2]]
     rhs = App "filter" [App "and" [Var 0, Var 1], Var 2]
     constrs = []
 
-flipBinop op = Rule {lhs, rhs, constrs}
+hoistFilter = alwaysRule lhs rhs constrs
+  where
+    lhs = App "join" [Var 0, App "filter" [Var 1, Var 2], Var 3]
+    rhs = App "filter" [Var 1, App "join" [Var 0, Var 2, Var 3]]
+    constrs = []
+
+toDepjoin = alwaysRule lhs rhs constrs
+  where
+    lhs = App "join" [Var 0, Var 1, Var 2]
+    rhs = App "depjoin" [Var 1, NodePat scope, App "filter" [Var 0, Var 2]]
+    constrs = []
+
+flipJoin = alwaysRule lhs rhs constrs
+  where
+    lhs = App "join" [Var 0, Var 1, Var 2]
+    rhs = App "join" [Var 0, Var 2, Var 1]
+    constrs = []
+
+flipBinop op = alwaysRule lhs rhs constrs
   where
     lhs = App op [Var 0, Var 1]
     rhs = App op [Var 1, Var 0]
@@ -156,15 +214,30 @@ flipBinop op = Rule {lhs, rhs, constrs}
 flipEq = flipBinop "eq"
 flipAnd = flipBinop "and"
 
-rewrite = repeat' 20
-          $ andThen (try $ rewriteFirst splitFilter)
-          $ andThen (try $ rewriteFirst mergeFilter)
-          $ (try $ rewriteFirst filterToHidx)
+rewrite =
+  repeat' 5 $
+  try (rewriteFirst splitFilter) `andThen`
+  try (rewriteFirst mergeFilter) `andThen`
+  try (rewriteFirst hoistFilter) `andThen`
+  try (rewriteFirst filterToOidx) `andThen`
+  try (rewriteFirst filterToOidx1) `andThen`
+  try (rewriteFirst filterToOidx2) `andThen`
+  try (rewriteFirst filterToHidx) `andThen`
+  try (rewriteFirst toDepjoin) `andThen`
+  try (rewriteFirst flipAnd) `andThen`
+  try (rewriteFirst flipEq) `andThen`
+  try (rewriteFirst flipJoin)
 
-tpch3 = join (eq (name "c_custkey") (name "o_custkey")) 
-        (join (eq (name "l_orderkey") (name "o_orderkey")) 
+relationSchema "orders" = ["o_orderkey", "o_custkey", "o_orderstatus", "o_totalprice", "o_orderdate", "o_orderpriority", "o_clerk", "o_shippriority", "o_comment"]
+relationSchema "customer" = ["c_custkey", "c_name", "c_address", "c_nationkey", "c_phone", "c_acctbal", "c_mktsegment", "c_comment"]
+relationSchema "lineitem" = ["l_orderkey", "l_partkey", "l_suppkey", "l_linenumber", "l_quantity", "l_extendedprice", "l_discount", "l_tax", "l_returnflag", "l_linestatus", "l_shipdate", "l_commitdate", "l_receiptdate", "l_shipinstruct", "l_shipmode", "l_comment"]
+
+schema n = undefined
+
+tpch3 = join (eq (name "c_custkey") (name "o_custkey"))
+        (join (eq (name "l_orderkey") (name "o_orderkey"))
           (filter' (lt (name "o_orderdate") (name "param1")) (relation "orders"))
           (filter' (lt (name "param1") (name "l_shipdate")) (relation "lineitem")))
         (filter' (eq (name "c_mktsegment") (name "param0")) (relation "customer"))
-          
+
 dumpDot (Just x) = Dot.prettyPrintDot $ toDot x
