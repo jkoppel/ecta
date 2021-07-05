0ff1639a1e94a3af4f3db6ca658a89a8f2322573
Jack Feser <feser@csail.mit.edu>
2021-07-05 18:10:32 -0400

    Start e-graph baseline.

--- src/DbOpt.hs
+++ src/DbOpt.hs
@@ -40,12 +40,13 @@ name = const'
 relation = const'
 scope = consts ["k1", "k2", "k3"]
 hidx x y z = func "hidx" [x, y, z]
-meta = unary "meta"
 tNode = const' "T"
 fNode = const' "F"
 
 data Pattern p =
   App Symbol [p]
+  | Meta p
+  | As (p, Int)
   | Var Int
   | NodePat Node
   deriving ( Eq, Ord, Show )
@@ -64,17 +65,25 @@ matchRoot p n =
   List.map (\s -> PatternMatch { root = n, pat = p, subst = s }) $ matches p n
   where
     matches :: ConstrPattern -> Node -> [[(Int, Node)]]
-    matches (ConstrPattern (p, eqs)) n =
+    matches (ConstrPattern (p, _)) n =
       case p of
         (App sym args) ->
-          List.concatMap (\pats ->
-                             (List.map concat $
-                              mapM (uncurry matches) pats :: [[(Int, Node)]])) $
-          List.map (\(Edge _ nodes) -> zip args nodes) $
+          matchArgs args $
           List.filter (\(Edge sym' _) -> sym == sym') $
           nodeEdges n
+        (As (p, idx)) -> List.map (\m -> (idx, n) : m) $ matches p n
+        (Meta p) -> matchMeta p $ nodeEdges n
         (Var idx) -> [[(idx, n)]]
         (NodePat n') -> if n == n' then [[]] else []
+
+    matchArgs args edges =
+      concatMap ((List.map concat . mapM (uncurry matches))
+                 . (\(Edge _ nodes) -> zip args nodes)) edges
+
+    matchMeta pat edges =
+      concatMap ((List.map concat . mapM (uncurry matches))
+                 . (\(Edge _ nodes) -> [(pat, head nodes)])) edges
+
 
 match :: ConstrPattern -> Node -> [PatternMatch]
 match p = crush (matchRoot p)
@@ -98,6 +107,8 @@ liftPat subst (ConstrPattern (pat, constrs)) =
     (App sym args) -> funcC sym (List.map (liftPat subst) args) constrs
     (Var idx) -> Maybe.fromJust $ List.lookup idx subst
     (NodePat n) -> n
+    (As _) -> error "Cannot lift As"
+    (Meta _) -> error "Cannot lift Meta"
 
 liftClosedPat = liftPat []
 
@@ -154,13 +165,15 @@ repeat' i r n =
     Just n' ->
       if n == n' then Just n else repeat' (i - 1) r n'
 
-alwaysRule lhs rhs = Rule { lhs, rhs, DbOpt.pred = const True }
+rule lhs rhs = Rule { lhs, rhs, DbOpt.pred = const True }
 
 var x = ConstrPattern (Var x, EmptyConstraints)
 varC x c = ConstrPattern (Var x, c)
 app f xs = ConstrPattern (App f xs, EmptyConstraints)
 appC f xs c = ConstrPattern (App f xs, c)
 nodePat n = ConstrPattern (NodePat n, EmptyConstraints)
+meta p = ConstrPattern (Meta p, EmptyConstraints)
+as p i = ConstrPattern (As (p, i), EmptyConstraints)
 
 andConstr p p' =
   (nodePat node, constr)
@@ -180,7 +193,7 @@ copyConstr p =
   where
     node = Node [ tEdge, fEdge ]
     constr = mkEqConstraints [[path [0, 0], p]]
-    
+
 bothT name x y = appC name [metaNode, x, y] constrs
   where
     (metaNode, constrs) = andConstr (path [1, 0, 0]) (path [2, 0, 0])
@@ -218,7 +231,7 @@ addConstr c = combineEqConstraints (mkEqConstraints c)
 idxT name xs = appC name (metaNode : xs) constrs
   where
     (metaNode, constrs) = copyConstr (path [3, 0, 0])
-    
+
 hidxT = idxT "hidx"
 oidxT = idxT "oidx"
 
@@ -229,7 +242,7 @@ eqP x y = app "eq" [var (-1), x, y]
 dotP x y = app "dot" [var (-1), x, y]
 joinP x y z = app "join" [var (-1), x, y, z]
 
-filterToHidx = alwaysRule lhs rhs
+filterToHidx = rule lhs rhs
   where
     ckey = var 0
     rkey = var 1
@@ -237,7 +250,7 @@ filterToHidx = alwaysRule lhs rhs
     lhs = filterP (eqP ckey rkey) rel
     rhs = hidxT [selectT ckey rel, cscopeT, filterT (eqT ckey (nodePat scope `dotT` ckey)) rel, rkey]
 
-filterToOidx = alwaysRule lhs rhs 
+filterToOidx = rule lhs rhs
   where
     ckey = var 0
     rkey1 = var 1
@@ -250,7 +263,7 @@ filterToOidx = alwaysRule lhs rhs
                   rkey1, rkey2
                 ]
 
-filterToOidx1 = alwaysRule lhs rhs 
+filterToOidx1 = rule lhs rhs
   where
     ckey = var 0
     rkey = var 1
@@ -261,7 +274,7 @@ filterToOidx1 = alwaysRule lhs rhs
                   filterT (ckey `eqT` (nodePat scope `dotT` ckey)) rel,
                   app "none" [], rkey]
 
-filterToOidx2 = alwaysRule lhs rhs 
+filterToOidx2 = rule lhs rhs
   where
     ckey = var 0
     rkey = var 1
@@ -272,70 +285,67 @@ filterToOidx2 = alwaysRule lhs rhs
                   filterT (ckey `eqT` (nodePat scope `dotT` ckey)) rel,
                   rkey, app "none" []]
 
-splitFilter = alwaysRule lhs rhs 
+splitFilter = rule lhs rhs
   where
     lhs = filterP (var 0 `andP` var 1) (var 2)
     rhs = filterT (var 0) $ filterT (var 1) (var 2)
 
-mergeFilter = alwaysRule lhs rhs 
+mergeFilter = rule lhs rhs
   where
     lhs = filterP (var 0) $ filterP (var 1) (var 2)
     rhs = filterT (var 0 `andT` var 1) (var 2)
 
-hoistFilter = alwaysRule lhs rhs 
+hoistFilter = rule lhs rhs
   where
     lhs = joinP (var 0) (filterP (var 1) (var 2)) (var 3)
     rhs = filterT (var 1) $ joinT (var 0) (var 2) (var 3)
 
-elimJoin = alwaysRule lhs rhs 
+elimJoin = rule lhs rhs
   where
     lhs = joinP (var 0) (var 1) (var 2)
     rhs = depjoinT (var 1) (filterT (var 0) (var 2))
 
-flipJoin = alwaysRule lhs rhs 
+flipJoin = rule lhs rhs
   where
     lhs = joinP (var 0) (var 1) (var 2)
     rhs = joinT (var 0) (var 2) (var 1)
 
-flipBinop op = alwaysRule lhs rhs
+flipBinop op = rule lhs rhs
   where
     lhs = app op [var 0, var 1]
     rhs = app op [var 1, var 0]
 
-forceRtime = alwaysRule (var 0) (appC "constrained" [rtimeT, var 0] (mkEqConstraints [[path [0, 0], path [1, 0, 0]]]))
+forceRtime = rule (var 0) (appC "constrained" [rtimeT, var 0] (mkEqConstraints [[path [0, 0], path [1, 0, 0]]]))
 
 flipEq = flipBinop "eq"
 flipAnd = flipBinop "and"
 
-rewrite t =
-  fmap reducePartially $
-  (repeat' 5 $
-  try (rewriteAll splitFilter) `andThen`
-  try (rewriteAll mergeFilter) `andThen`
-  try (rewriteAll hoistFilter) `andThen`
-  try (rewriteAll filterToOidx) `andThen`
-  try (rewriteAll filterToOidx1) `andThen`
-  try (rewriteAll filterToOidx2) `andThen`
-  try (rewriteAll filterToHidx) `andThen`
-  try (rewriteAll elimJoin) `andThen`
-  try (rewriteAll flipAnd) `andThen`
-  try (rewriteAll flipEq) `andThen`
-  try (rewriteAll flipJoin)) `andThen`
-  (rewriteRoot forceRtime) $ liftClosedPat t
+-- rewrite t =
+--   fmap reducePartially $
+--   (repeat' 5 $
+--   try (rewriteAll splitFilter) `andThen`
+--   try (rewriteAll mergeFilter) `andThen`
+--   try (rewriteAll hoistFilter) `andThen`
+--   try (rewriteAll filterToOidx) `andThen`
+--   try (rewriteAll filterToOidx1) `andThen`
+--   try (rewriteAll filterToOidx2) `andThen`
+--   try (rewriteAll filterToHidx) `andThen`
+--   try (rewriteAll elimJoin) `andThen`
+--   try (rewriteAll flipAnd) `andThen`
+--   try (rewriteAll flipEq) `andThen`
+--   try (rewriteAll flipJoin)) `andThen`
+--   (rewriteRoot forceRtime) $ liftClosedPat t
 
-rewriteTest t =
+rewriteNoReduce t =
   fmap reducePartially
   $ (repeat' 5 $
      try (rewriteAll elimJoin)
      `andThen` try (rewriteAll filterToHidx)
-             -- `andThen` try (rewriteAll flipJoin) `andThen`
-             -- try (rewriteAll splitFilter) `andThen`
-             -- -- try (rewriteAll mergeFilter) `andThen`
-             -- try (rewriteAll hoistFilter)
-             -- -- `andThen` try (rewriteAll filterToHidx)
     )
-  `andThen` (rewriteRoot forceRtime)
+  `andThen` rewriteRoot forceRtime
   $ liftClosedPat t
+
+rewrite t = reducePartially <$> rewriteNoReduce t
 
 schema "orders" _ = ["o_orderkey", "o_custkey", "o_orderstatus", "o_totalprice", "o_orderdate", "o_orderpriority", "o_clerk", "o_shippriority", "o_comment"]
 schema "customer" _ = ["c_custkey", "c_name", "c_address", "c_nationkey", "c_phone", "c_acctbal", "c_mktsegment", "c_comment"]
@@ -366,3 +376,68 @@ ntuples "orders" _ = 1500000
 ntuples "customer" _ = 150000
 ntuples "constrained" xs = minimum xs
 ntuples _ _ = 0
+
+rtimeE = app "runtime" []
+ctimeE = app "comptime" []
+relationE n = app n [rtimeE]
+joinE p r r' = app "join" [ctimeT, p, r, r']
+nameE n = app n [rtimeE]
+eqE t p p' = app "eq" [t, p, p']
+ltE t p p' = app "lt" [t, p, p']
+dotE t p p' = app "dot" [t, p, p']
+filterE t p q = app "filter" [t, p, q]
+depjoinE t p q = app "depjoin" [t, p, q]
+hidxE t p s q k = app "hidx" [t, p, s, q, k]
+selectE t p q = app "select" [t, p, q]
+cscopeE = app "k" [ctimeE]
+rscopeE = app "k" [rtimeE]
+
+tpch3E = joinE (eqE rtimeE (nameE "c_custkey") (nameE "o_custkey"))
+        (joinE (eqE rtimeE (nameE "l_orderkey") (nameE "o_orderkey"))
+          (filterE rtimeE (ltE rtimeE (nameE "o_orderdate") (nameE "param1")) (relationE "orders"))
+          (filterE rtimeE (ltE rtimeE (nameE "param1") (nameE "l_shipdate")) (relationE "lineitem")))
+        (filterE rtimeE (eqE rtimeE (nameE "c_mktsegment") (nameE "param0")) (relationE "customer"))
+
+elimJoinE = rule lhs rhs
+  where
+    lhs = joinP (var 0) (var 1) (var 2)
+    rhs = depjoinE ctimeE (var 1) (filterE ctimeE (var 0) (var 2))
+
+depjoinStageE = rule lhs rhs
+  where
+    lhs = depjoinE (var 0) (meta rtimeE `as` 1) (meta rtimeE `as` 2)
+    rhs = depjoinE rtimeE (var 1) (var 2)
+
+filterStageE = rule lhs rhs
+  where
+    lhs = filterP (meta rtimeE `as` 1) (meta rtimeE `as` 2)
+    rhs = filterE rtimeE (var 1) (var 2)
+
+eqStageE = rule lhs rhs
+  where
+    lhs = eqP (meta rtimeE `as` 1) (meta rtimeE `as` 2)
+    rhs = eqE rtimeE (var 1) (var 2)
+
+hidxStageE = rule lhs rhs
+  where
+    lhs = hidxE (var 0) (var 1) (var 2) (meta rtimeE `as` 3) (var 4)
+    rhs = hidxE rtimeE (var 1) (var 2) (var 3) (var 4)
+
+filterToHidxE = rule lhs rhs
+  where
+    ckey = var 0
+    rkey = var 1
+    rel = var 2
+    lhs = filterP (eqP ckey rkey) rel
+    rhs = hidxE ctimeE (selectE ctimeE ckey rel) cscopeE (filterE ctimeE (eqE ctimeE ckey (dotE ctimeE cscopeE ckey)) rel) rkey
+
+rewriteEgraph t =
+  repeat' 5
+  (
+    try (rewriteAll elimJoinE)
+    `andThen` try (rewriteAll filterToHidxE)
+    `andThen` try (rewriteAll depjoinStageE)
+    `andThen` try (rewriteAll filterStageE)
+    `andThen` try (rewriteAll eqStageE)
+    `andThen` try (rewriteAll hidxStageE)
+  ) $ liftClosedPat t
