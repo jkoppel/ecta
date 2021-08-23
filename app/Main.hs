952f89f8e07e88b10981c681ffdb92757ce82752
Zheng Guo <zhg069@eng.ucsd.edu>
2021-08-23 01:53:51 -0700

    customized graph

--- app/Main.hs
+++ app/Main.hs
@@ -7,25 +7,17 @@ import Data.List ( nub )
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import System.IO ( hFlush, stdout )
-import Data.Time
-import Text.Pretty.Simple
-import Language.Dot.Pretty
 
 import Data.ECTA
 import Data.ECTA.Internal.ECTA.Enumeration
 import Data.ECTA.Term
 import Data.Persistent.UnionFind
-import Data.Text.Extended.Pretty
 import TermSearch
-import Utility.Fixpoint
 
 ----------------------------------------------------------
 
 printAllEdgeSymbols :: Node -> IO ()
 printAllEdgeSymbols n = print $ nub $ crush (onNormalNodes $ \(Node es) -> map edgeSymbol es) n
-
-reduceFully :: Node -> Node
-reduceFully = fixUnbounded (withoutRedundantEdges . reducePartially)
 
 printCacheStatsForReduction :: Node -> IO ()
 printCacheStatsForReduction n = do
@@ -48,28 +40,20 @@ getTermsNoOccursCheck n = map (termFragToTruncatedTerm . fst) $
                             _ <- enumerateOutUVar (intToUVar 0)
                             getTermFragForUVar    (intToUVar 0)
 
-prettyPrintAllTerms :: Node -> IO ()
-prettyPrintAllTerms n = let ts = map pretty $ map prettyTerm $ getAllTerms n
-                        in do pPrint ts
-                              print (length ts)
-#ifdef PROFILE_CACHES
-                              Memoization.printAllCacheMetrics
-                              Text.putStrLn =<< (pretty <$> Interned.getMetrics (cache @Node))
-                              Text.putStrLn =<< (pretty <$> Interned.getMetrics (cache @Edge))
-                              Text.putStrLn ""
-#endif
+
 
 main :: IO ()
 main = do
-    start <- getCurrentTime
-    let !filterNode = filterType (relevantTermsUptoK 6) baseType
-    middle1 <- getCurrentTime
-    print $ "Filter type time: " ++ show (diffUTCTime middle1 start)
-    -- let !node = filterArgs filterNode 
-    let !node = filterNode
-    middle <- getCurrentTime
-    print $ "Construction time: " ++ show (diffUTCTime middle start)
-    prettyPrintAllTerms $ refold $ reduceFully node
-    end <- getCurrentTime
-    print $ "Reduction time: " ++ show (diffUTCTime end middle)
+    -- start <- getCurrentTime
+    -- let !filterNode = filterType (relevantTermsUptoK 6) baseType
+    -- middle1 <- getCurrentTime
+    -- print $ "Filter type time: " ++ show (diffUTCTime middle1 start)
+    -- -- let !node = filterArgs filterNode 
+    -- let !node = filterNode
+    -- middle <- getCurrentTime
+    -- print $ "Construction time: " ++ show (diffUTCTime middle start)
+    -- prettyPrintAllTerms $ refold $ reduceFully node
+    -- end <- getCurrentTime
+    -- print $ "Reduction time: " ++ show (diffUTCTime end middle)
     -- putStrLn $ renderDot . toDot $ node
+    mapM_ runBenchmark benchmarks
