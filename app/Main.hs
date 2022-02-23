5ad5f9503f1c358540759921e7390a9b1a97fae6
Zheng Guo <zhg069@eng.ucsd.edu>
2022-02-22 16:33:07 -0800

    add ablation flag

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,14 @@ import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import System.IO ( hFlush, stdout )
 
-import System.Console.CmdArgs ( Data, Typeable, cmdArgs, argPos, auto, (&=) )
+import System.Console.CmdArgs ( Data, Typeable, cmdArgs, argPos, auto, (&=), help )
 
 import Data.ECTA
 import Data.ECTA.Internal.ECTA.Enumeration
 import Data.ECTA.Term
 import Data.Persistent.UnionFind
 import Application.TermSearch.Evaluation
+import Application.TermSearch.Type
 
 ----------------------------------------------------------
 
@@ -45,16 +46,18 @@ getTermsNoOccursCheck n = map (termFragToTruncatedTerm . fst) $
 --------------------------------------------------------------------------------
 
 data HPPArgs = HPPArgs { benchmark :: String
+                       , ablation  :: AblationType
                        }
   deriving (Data, Typeable)
 
 hppArgs :: HPPArgs
 hppArgs = HPPArgs {
     benchmark = "" &= argPos 0
+  , ablation  = Default &= help "Ablation type. choices: [default, no-reduction, no-enumeration]"
   } &= auto
 
 
 main :: IO ()
 main = do
     query <- cmdArgs hppArgs
-    runBenchmark (read $ benchmark query)
+    runBenchmark (read $ benchmark query) (ablation query)
