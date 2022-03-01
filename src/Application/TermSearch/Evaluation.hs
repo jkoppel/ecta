221b31a58c63d020848901a50242ea1994ee1c2a
Zheng Guo <zhg069@eng.ucsd.edu>
2022-03-01 15:42:33 -0800

    loop over different term sizes

--- src/Application/TermSearch/Evaluation.hs
+++ src/Application/TermSearch/Evaluation.hs
@@ -3,6 +3,7 @@ module Application.TermSearch.Evaluation
     , runBenchmark
     ) where
 
+import           Control.Monad                  ( forM_ )
 import           Data.Time                      ( diffUTCTime
                                                 , getCurrentTime
                                                 )
@@ -23,32 +24,33 @@ import           Application.TermSearch.Type
 import           Application.TermSearch.Utils
 
 runBenchmark :: Benchmark -> AblationType -> Int -> IO ()
-runBenchmark (Benchmark name depth sol args res) ablation limit = do
-    start <- getCurrentTime
+runBenchmark (Benchmark name size sol args res) ablation limit = do
     putStrLn $ "Running benchmark " ++ Text.unpack name
+
     let argNodes = map (Bi.bimap Symbol typeToFta) args
     let resNode  = typeToFta res
-    let anyArg   = Node (map (uncurry constArg) argNodes)
-    let
-        !filterNode = filterType
-            (relevantTermsUptoK anyArg argNodes depth)
-            resNode
 
-    _ <- timeout (limit * 10 ^ (6 :: Int)) $ do
-        case ablation of
-            NoReduction -> do
-                prettyPrintAllTerms ablation (substTerm sol) filterNode
-            NoOptimize  -> do
-                prettyPrintAllTerms ablation (substTerm sol) filterNode
-            _           -> do
-                reducedNode <- reduceFullyAndLog filterNode
-                -- let reducedNode = reduceFully filterNode
-                let foldedNode = refold reducedNode
-                prettyPrintAllTerms ablation (substTerm sol) foldedNode
-
+    start <- getCurrentTime
+    _ <- timeout (limit * 10 ^ (6 :: Int)) $ forM_ [1..size] $ synthesize argNodes resNode
     end <- getCurrentTime
     print $ "Time: " ++ show (diffUTCTime end start)
     hFlush stdout
+
+  where
+    synthesize :: [Argument] -> Node -> Int -> IO ()
+    synthesize argNodes resNode sz = do
+      let anyArg   = Node (map (uncurry constArg) argNodes)
+      let !filterNode = filterType (relevantTermsOfSize anyArg argNodes sz) resNode
+      case ablation of
+          NoReduction -> do
+              prettyPrintAllTerms ablation (substTerm sol) filterNode
+          NoOptimize  -> do
+              prettyPrintAllTerms ablation (substTerm sol) filterNode
+          _           -> do
+              reducedNode <- reduceFullyAndLog filterNode
+              -- let reducedNode = reduceFully filterNode
+              let foldedNode = refold reducedNode
+              prettyPrintAllTerms ablation (substTerm sol) foldedNode
 
 runEval :: IO ()
 runEval = undefined -- mapM_ runBenchmark hoogleplusBenchmarks
