31abd6efff1d0cc11488ee43fbce0efafea22cc9
Zheng Guo <zhg069@eng.ucsd.edu>
2022-03-07 19:18:58 -0800

    copy Equality saturation from jk-lambda

--- src/Application/EqualitySaturation.hs
+++ src/Application/EqualitySaturation.hs
@@ -1,7 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module EqualitySaturation (
-
+module Application.EqualitySaturation (
+    At (..)
+  , UseVarAt (..)
+  , useVar
+  , maxSupportedInt
+  , nodeModEcs
+  , modEcs
+  , varPath
+  , copyEnvToChild
+  , copyEnvToChildren
+  , copyLiftedEnvToChild
   ) where
 
 import           Data.List ( (\\) )
@@ -54,8 +63,8 @@ envPath = path [0]
 childPath :: Int -> Path
 childPath childIdx = path [childIdx + 1]
 
-varPath :: Int -> Path
-varPath varIdx = envPath <> path [varIdx]
+varPath :: Path -> Int -> Path
+varPath ep varIdx = ep <> path [varIdx]
 
 ----- Env usage
 
@@ -66,24 +75,24 @@ data At = At
 useVar :: Int -> At -> Int -> UseVarAt
 useVar varIdx _ childIdx = UseVarAt varIdx childIdx
 
-copyEnvToChild :: Int -> EqConstraints
-copyEnvToChild childIdx = mkEqConstraints [[envPath, childPath childIdx <> envPath]]
+copyEnvToChild :: Path -> Int -> EqConstraints
+copyEnvToChild ep childIdx = mkEqConstraints [[ep, childPath childIdx <> ep]]
 
-copyEnvToChildren :: Int -> EqConstraints
-copyEnvToChildren numChildren = foldMap copyEnvToChild [0..numChildren - 1]
+copyEnvToChildren :: Path -> Int -> EqConstraints
+copyEnvToChildren ep numChildren = foldMap (copyEnvToChild ep) [0..numChildren - 1]
 
-withCopyEnvToChildren :: Int -> Node -> Node
-withCopyEnvToChildren numChildren n = nodeMapChildren (modEcs (<> copyEnvToChildren numChildren)) n
+withCopyEnvToChildren :: Path -> Int -> Node -> Node
+withCopyEnvToChildren ep numChildren n = nodeMapChildren (modEcs (<> copyEnvToChildren ep numChildren)) n
 
-useVarFromEnv :: UseVarAt -> Edge -> Edge
-useVarFromEnv uva e = modEcs (<> mkEqConstraints [[varPath (varIdx uva), childPath (childIdx uva)]]) e
+useVarFromEnv :: Path -> UseVarAt -> Edge -> Edge
+useVarFromEnv ep uva e = modEcs (<> mkEqConstraints [[varPath ep (varIdx uva), childPath (childIdx uva)]]) e
 
-nodeUseVarFromEnv :: UseVarAt -> Node -> Node
-nodeUseVarFromEnv uva n = nodeMapChildren (useVarFromEnv uva) n
+nodeUseVarFromEnv :: Path -> UseVarAt -> Node -> Node
+nodeUseVarFromEnv ep uva n = nodeMapChildren (useVarFromEnv ep uva) n
 
-withEnvPropagation :: Int -> [UseVarAt] -> Node -> Node
-withEnvPropagation numChildren uvas n = nodeModEcs (<> (foldMap copyEnvToChild normalChildren))
-                                                   $ foldr nodeUseVarFromEnv n uvas
+withEnvPropagation :: Path -> Int -> [UseVarAt] -> Node -> Node
+withEnvPropagation ep numChildren uvas n = nodeModEcs (<> (foldMap (copyEnvToChild ep) normalChildren))
+                                                      $ foldr (nodeUseVarFromEnv ep) n uvas
   where
     varChildren = map childIdx uvas
     normalChildren = [0..numChildren - 1] \\ varChildren
@@ -91,11 +100,11 @@ withEnvPropagation numChildren uvas n = nodeModEcs (<> (foldMap copyEnvToChild n
 
 ----- de Bruijn index operations
 
-copyLiftedEnvToChild :: Int -> EqConstraints
-copyLiftedEnvToChild childIdx = foldMap doCopyVar [1..envSize - 1]
+copyLiftedEnvToChild :: Path -> Int -> Int -> EqConstraints
+copyLiftedEnvToChild ep sz childIdx = foldMap doCopyVar [1..sz - 1]
   where
     doCopyVar :: Int -> EqConstraints
-    doCopyVar targIdx = mkEqConstraints [[varPath (targIdx - 1), childPath childIdx <> varPath (targIdx)]]
+    doCopyVar targIdx = mkEqConstraints [[varPath ep (targIdx - 1), childPath childIdx <> varPath ep targIdx]]
 
 ------------------
 ------ Node types
@@ -105,7 +114,7 @@ copyLiftedEnvToChild childIdx = foldMap doCopyVar [1..envSize - 1]
 --            we use is known to have nonterminating cases for such nodes. Fingers crossed
 --            such problems will not come up.
 anyNode :: Node
-anyNode = createGloballyUniqueMu (\n -> union (map (Node . (:[]) . constructorToEdge n) constructors))
+anyNode = createMu (\n -> union (map (Node . (:[]) . constructorToEdge n) constructors))
   where
     mkAnyEnvNode :: Node -> Node
     mkAnyEnvNode n = Node [Edge "env" $ replicate envSize n]
@@ -117,41 +126,43 @@ anyNode = createGloballyUniqueMu (\n -> union (map (Node . (:[]) . constructorTo
     intConstructors = map (\n -> (Text.pack $ show n, 0)) [0..maxSupportedInt]
     usedConstructors = [("lambda", 1), ("apply", 2), ("+", 2)]
 
-anyEnv :: Node
-anyEnv = Node [Edge "env" $ replicate envSize anyNode]
+anyEnv :: Node -> Node
+anyEnv tau = Node [Edge "env" $ replicate envSize tau]
 
 intNode :: Int -> Node
 intNode n
   | n > maxSupportedInt = error $ "intNode: int out of range " <> show n
-intNode n               = Node [Edge (Symbol $ Text.pack $ show n) [anyEnv]]
+intNode n               = Node [Edge (Symbol $ Text.pack $ show n) [anyEnv anyNode]]
 
 plusRaw :: Node -> Node -> Node
-plusRaw n1 n2 = Node [Edge "+" [anyEnv, n1, n2]]
+plusRaw n1 n2 = Node [Edge "+" [anyEnv anyNode, n1, n2]]
 
-plus :: Node -> Node -> Node
-plus n1 n2 = withCopyEnvToChildren 2 (plus n1 n2)
+plus :: Path -> Node -> Node -> Node
+plus ep n1 n2 = withCopyEnvToChildren ep 2 (plus ep n1 n2)
 
-lambda :: Node -> Node
-lambda n = Node [mkEdge "lambda" [anyEnv, n] (copyLiftedEnvToChild 0)]
+lambda :: Path -> Node -> Node
+lambda ep n = Node [mkEdge "lambda" [anyEnv anyNode, n] (copyLiftedEnvToChild ep envSize 0)]
 
 applyRaw :: Node -> Node -> Node
-applyRaw fn arg = Node [Edge "apply" [anyEnv, fn, arg]]
+applyRaw fn arg = Node [Edge "apply" [anyEnv anyNode, fn, arg]]
 
-apply :: Node -> Node -> Node
-apply fn arg = nodeModEcs (<> copyEnvToChild 1) $ applyRaw fn arg
+apply :: Path -> Node -> Node -> Node
+apply ep fn arg = nodeModEcs (<> copyEnvToChild ep 1) $ applyRaw fn arg
 
 ------------------
 ------ Terms of interest
 ------------------
 
 plus1 :: Node
-plus1 = lambda (withEnvPropagation 2 [useVar 0 At 0] $ plusRaw anyNode (intNode 1))
+plus1 = lambda envPath (withEnvPropagation envPath 2 [useVar 0 At 0] $ plusRaw anyNode (intNode 1))
 
 compose :: Node
-compose = lambda $ lambda $ lambda
-                          $ withEnvPropagation 2 [useVar 2 At 0] $ applyRaw anyNode
-                                                                            $ withEnvPropagation 2 [useVar 1 At 0, useVar 0 At 1]
-                                                                                                 $ applyRaw anyNode anyNode
+compose = lambda envPath 
+                 $ lambda envPath 
+                          $ lambda envPath
+                                   $ withEnvPropagation envPath 2 [useVar 2 At 0] $ applyRaw anyNode
+                                                                                             $ withEnvPropagation envPath 2 [useVar 1 At 0, useVar 0 At 1]
+                                                                                                                  $ applyRaw anyNode anyNode
 
 ------------------------------------------------------------
 ------------------------ Rewriting -------------------------
